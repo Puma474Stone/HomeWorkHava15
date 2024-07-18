0b1c8f9e2f56645a5eedba7f837545504860f608
Puma474Stone <shustov.arcadi@yandex.ru>
2024-07-18 13:51:09 +0700

    AviaSouls

---
+++ src/test/java/ru/netology/statistic/AviaSoulsTest.java
@@ -0,0 +1,57 @@
+package ru.netology.statistic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AviaSoulsTest {
+
+    @Test
+    public void testAddAndFindAll() {
+        AviaSouls manager = new AviaSouls();
+        Ticket ticket1 = new Ticket("Moscow", "London", 100, 5, 7);
+        Ticket ticket2 = new Ticket("Moscow", "Paris", 200, 6, 8);
+
+        manager.add(ticket1);
+        manager.add(ticket2);
+
+        Ticket[] expected = {ticket1, ticket2};
+        Ticket[] actual = manager.findAll();
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testSearch() {
+        AviaSouls manager = new AviaSouls();
+        Ticket ticket1 = new Ticket("Moscow", "London", 100, 5, 7);
+        Ticket ticket2 = new Ticket("Moscow", "London", 50, 6, 8);
+        Ticket ticket3 = new Ticket("Moscow", "Paris", 200, 6, 8);
+
+        manager.add(ticket1);
+        manager.add(ticket2);
+        manager.add(ticket3);
+
+        Ticket[] expected = {ticket2, ticket1};
+        Ticket[] actual = manager.search("Moscow", "London");
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testSearchAndSortBy() {
+        AviaSouls manager = new AviaSouls();
+        Ticket ticket1 = new Ticket("Moscow", "London", 100, 5, 7);
+        Ticket ticket2 = new Ticket("Moscow", "London", 50, 6, 9);
+        Ticket ticket3 = new Ticket("Moscow", "Paris", 200, 6, 8);
+
+        manager.add(ticket1);
+        manager.add(ticket2);
+        manager.add(ticket3);
+
+        TicketTimeComparator comparator = new TicketTimeComparator();
+
+        Ticket[] expected = {ticket1, ticket2};
+        Ticket[] actual = manager.searchAndSortBy("Moscow", "London", comparator);
+        assertArrayEquals(expected, actual);
+    }
+}
+
